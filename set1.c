48cf6b1c35f88a6ad1e2f956f06090993381d67f
mowleedharan <35652470+mowleedharan@users.noreply.github.com>
2018-01-21 16:11:29 +0530

    Create set1.c

---
+++ set1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a;
+  printf("enter a value");
+  scanf("%d",&a);
+  if(a>0)
+{
+  printf("the value is positive");
+}
+else if(a<0)
+{
+  printf("the value is negative");
+}
+else(a=0)
+{
+  printf("the value is zero");
+}
+}
